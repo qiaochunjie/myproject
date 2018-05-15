18d54fc15b46ac4e93b81694a1648d4ef1221724
qcj <1414680811@qq.com>
2018-05-15 17:37:40 +0800

    Initial commit

---
+++ javademo/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static  void  main(String[] args)
+    {
+        //region wew
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        //endregion
+    }
+}
---
+++ webdemo/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: qcj6900
+  Date: 2017/11/29
+  Time: 19:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>标题</title>
+  </head>
+  <body>
+  Hello Java Web !!!
+  </body>
+</html>
