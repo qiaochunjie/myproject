c1c414df5d141c0a60c85e7d1c5ec7bbe43c9d17
qcj <1414680811@qq.com>
2018-06-05 16:44:39 +0800

    mymasterbranch

--- javademo/src/Main.java
+++ javademo/src/Main.java
@@ -3,13 +3,13 @@ public class Main {
     public static  void  main(String[] args)
     {
         //region wew
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hello world masterbranch");
+        System.out.println("hello world masterbranch");
+        System.out.println("hello world masterbranch");
+        System.out.println("hello world masterbranch");
+        System.out.println("hello world masterbranch");
+        System.out.println("hello world masterbranch");
+        System.out.println("hello world masterbranch");
         //endregion
     }
 }
